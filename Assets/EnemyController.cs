af926e7b8cdd0033a21efeaea7bad2b23b1c4de7
Hampus Fristedt <hampus.fristedt93@gmail.com>
2016-11-14 22:15:01 +0100

    Adds CanSeePlayer.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -7,10 +7,20 @@ public class EnemyController : Human {
     private GameObject player;
 
     new void Update() {
-        velocity = (player.transform.position - transform.position).normalized;
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg);
-        velocity *= MaxVel;
+        velocity = Vector3.zero;
+        if (CanSeePlayer()) {
+            velocity = (player.transform.position - transform.position).normalized;
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg);
+            velocity *= MaxVel;
+        }
         Shoot();
         base.Update();
+    }
+
+    bool CanSeePlayer() {
+        Vector3 dir = player.transform.position - transform.position;
+        Debug.DrawRay(transform.position, dir);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
+        return hit.collider.name == "Player";
     }
 }
