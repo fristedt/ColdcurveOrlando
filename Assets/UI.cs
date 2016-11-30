2b882ab2ab7d8364cf7ef76476bea8e14d355052
Hampus Fristedt <hampus.fristedt93@gmail.com>
2016-11-30 21:33:18 +0100

    Adds crosshair.

---
+++ Assets/UI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+    public Texture2D crosshairTexture;
+    private const float CrosshairSize = 32;
+    private Vector2 position = Vector3.zero;
+    private Vector2 size = new Vector2(CrosshairSize, CrosshairSize);
+
+    void OnGUI() {
+        Vector2 mousePos = Input.mousePosition;
+        position.x = mousePos.x - CrosshairSize / 2;
+        position.y = Screen.height - mousePos.y - CrosshairSize / 2;
+        Rect rect = new Rect(position, size);
+        GUI.DrawTexture(rect, crosshairTexture);
+    }
+}
